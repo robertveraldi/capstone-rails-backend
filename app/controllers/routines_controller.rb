282935ef5455687a7451374ef32943fba7ab2355
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-07 16:12:33 -0500

    routine create method

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,2 +1,14 @@
 class RoutinesController < ApplicationController
+  def create
+    routine = Routine.new!(
+      user_id: params[:user_id],
+      exercise_id: params[:exercise_id],
+      reps: params[:reps]
+    )
+
+    if routine.save
+      render json: {message: "Exercise added to routine."}
+    else
+      render json: {error: routine.error.full_messages}
+  end
 end
