7ab7862f38b2f0ef986c6e959de2f0ac18393423
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-07 15:28:50 -0500

    exercise show action

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,4 +3,9 @@ class ExercisesController < ApplicationController
     exercise = Exercise.all
     render json: exercise
   end
+
+  def show
+    exercise = Exercise.find_by(id: params[:id])
+    render json: exercise
+  end
 end
