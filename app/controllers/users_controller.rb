9f141ba35186459fb40ee1c414d81ce3b5e9e696
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-07 15:15:26 -0500

    user update action, route, http request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,18 @@ class UsersController < ApplicationController
       render json: { error: user.error.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.name = params[:name] || user.name
+    user.email = params[:email] || user.email
+    user.password_digest = params[:password_digest] || user.password_digest
+    user.image_url = params[:image_url] || user.image_url
+
+    if user.save
+      render json: user
+    else
+      render json: { error: user.error.full_messages }, status: :bad_request
+    end
+  end
 end
