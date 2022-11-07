9f464b85d0d3357f42f896c7323807b453d721ca
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-07 15:26:55 -0500

    exercises index route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "/users/:id" => "users#show"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#destroy"
+
+  get "/exercises" => "exercises#index"
 end
