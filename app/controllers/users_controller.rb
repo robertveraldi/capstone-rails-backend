d3dec26bad77a9d2ea8493e824a7497fb2987466
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-07 14:43:22 -0500

    user create function, added email validation in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create!(
+      name: params[:name],
+      email: params[:email],
+      password_digest: params[:password_digest],
+      image_url: params[:image_url],
+    )
+
+    if user.save
+      render json: {message: "User successfully created."}
+    else
+      render json: {error: user.error.full_messages}, status: :bad_request
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :exercises
   has_many :routines
+  validates :email, presence: true, uniqueness: true
 end
