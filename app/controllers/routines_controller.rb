8d775cbfe8cc9769b7eb941d159dfc3020618a4e
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-07 16:16:23 -0500

    routine create request, functional. added missing end statement to controller, and changed 'new' to 'create'

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,14 +1,15 @@
 class RoutinesController < ApplicationController
   def create
-    routine = Routine.new!(
+    routine = Routine.create!(
       user_id: params[:user_id],
       exercise_id: params[:exercise_id],
-      reps: params[:reps]
+      reps: params[:reps],
     )
 
     if routine.save
-      render json: {message: "Exercise added to routine."}
+      render json: { message: "Exercise added to routine." }
     else
-      render json: {error: routine.error.full_messages}
+      render json: { error: routine.error.full_messages }
+    end
   end
 end
